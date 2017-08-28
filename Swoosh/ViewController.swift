8be5c028a8a64ca264d271190f5ec92f2a654d61
Todd Fields <toddfields@Todds-Mac.local>
2017-08-28 15:53:58 -0500

    Second Commit

---
+++ Swoosh/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Swoosh
+//
+//  Created by Todd Fields on 2017-08-24.
+//  Copyright © 2017 Todd Fields. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    @IBOutlet weak var getStartedButton: BorderButton!
+    
+    // MARK: - Initialization
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func getStartedButtonPushed(_ sender: Any) {
+    }
+    
+}
+
