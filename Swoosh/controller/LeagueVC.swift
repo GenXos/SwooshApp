c2f89566c796be33effb3ff58eeeaf27811d25df
Todd Fields <toddwfields@gmail.com>
2017-09-08 10:59:02 -0500

    adjusted alpha on the skill level vc

--- Swoosh/controller/LeagueVC.swift
+++ Swoosh/controller/LeagueVC.swift
@@ -10,12 +10,41 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var nextButton: BorderButton!
+    
+    var player: Player!
+    
+    // MARK: - Initialization
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
-
+    
+    // MARK: - Functions
+    func selectLeague(leagueType: String) {
+        
+        player.desiredLeague = leagueType
+        nextButton.isEnabled = true
+    }
+    
+    // MARK: - IBActions
+    @IBAction func onMensTapped(_ sender: Any) {
+        
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+     
+        selectLeague(leagueType: "coed")
+    }
+    
     @IBAction func onNextTapped(_ sender: Any) {
         
         performSegue(withIdentifier: "skillVCSegue", sender: self)
---
+++ Swoosh/model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Todd Fields on 2017-08-29.
+//  Copyright © 2017 Todd Fields. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
